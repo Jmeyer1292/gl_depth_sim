f394b074435193e52e0bd41483aecb13783ccb62
Jonathan Meyer <jmeyer@swri.org>
2018-05-08 22:52:08 -0500

    Added impl for cv and plc conversion functions.

--- src/interfaces/opencv_interface.cpp
+++ src/interfaces/opencv_interface.cpp
@@ -2,10 +2,24 @@
 
 void gl_depth_sim::toCvImageFp(const gl_depth_sim::DepthImage& depth, cv::Mat& out)
 {
-
+  out.create(depth.rows, depth.cols, CV_32FC1);
+  for (int y = 0; y < depth.rows; ++y)
+  {
+    for (int x = 0; x < depth.cols; ++x)
+    {
+      out.at<float>(y,x) = depth.data[y * depth.cols + x];
+    }
+  }
 }
 
 void gl_depth_sim::toCvImage16u(const gl_depth_sim::DepthImage& depth, cv::Mat& out)
 {
-
+  out.create(depth.rows, depth.cols, CV_16UC1);
+  for (int y = 0; y < 600; ++y)
+  {
+    for (int x = 0; x < 800; ++x)
+    {
+      out.at<unsigned short>(y, x) = 1000.0f * depth.data[y * depth.cols + x];
+    }
+  }
 }
--- src/interfaces/pcl_interface.cpp
+++ src/interfaces/pcl_interface.cpp
@@ -3,4 +3,28 @@
 void gl_depth_sim::project(const gl_depth_sim::CameraProperties& camera, const gl_depth_sim::DepthImage& depth,
                            pcl::PointCloud<pcl::PointXYZ>& out)
 {
+  out.width = depth.cols;
+  out.height = depth.rows;
+  out.resize(out.width * out.height);
+  out.is_dense = false;
+
+  for (int i = 0; i < depth.rows; ++i)
+  {
+    for (int j = 0; j < depth.cols; ++j)
+    {
+      const float distance = depth.data[i * depth.cols + j];
+      pcl::PointXYZ& pt = out(j, i);
+
+      if (distance != 0.0f)
+      {
+        pt.z = distance;
+        pt.x = (j - camera.cx) * distance / camera.fx;
+        pt.y = (i - camera.cy) * distance / camera.fy;
+      }
+      else
+      {
+        pt.z = pt.x = pt.y = std::numeric_limits<float>::quiet_NaN();
+      }
+    }
+  } // end of loop
 }
