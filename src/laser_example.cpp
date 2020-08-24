29570b0d4b10a62e20480d7f38bfd1e2e02aded0
mripperger <michael.ripperger@swri.org>
2020-08-24 18:40:06 -0500

    Revised laser scan example

--- src/laser_example.cpp
+++ src/laser_example.cpp
@@ -1,122 +1,134 @@
-#include <ros/ros.h>
-#include <math.h>
-#include <chrono>
-#include <iostream>
+#include <gl_depth_sim/sim_laser_scanner.h>
+#include <gl_depth_sim/mesh_loader.h>
 
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl/io/pcd_io.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <visualization_msgs/Marker.h>
 
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
-#include <tf_conversions/tf_eigen.h>
-
-#include "gl_depth_sim/sim_depth_camera.h"
-#include <gl_depth_sim/sim_laser_scanner.h>
-#include "gl_depth_sim/sim_depth_camera.h"
-#include "gl_depth_sim/mesh_loader.h"
-#include "gl_depth_sim/interfaces/pcl_interface.h" // For converting to PCL cloud
-#include "gl_depth_sim/renderable_mesh.h"
-#include "gl_depth_sim/interfaces/opencv_interface.h"
-#include "gl_depth_sim/interfaces/pcl_interface.h"
-
-
-#include <opencv2/highgui/highgui.hpp>
-
-#include <Eigen/Dense>
-
-
-static Eigen::Isometry3d lookat(const Eigen::Vector3d& origin,
-                                const Eigen::Vector3d& eye,
-                                const Eigen::Vector3d& up)
+template<typename T>
+bool get(const ros::NodeHandle &nh, const std::string &key, T &val)
 {
-  Eigen::Vector3d z = (eye - origin).normalized();
-  Eigen::Vector3d x = z.cross(up).normalized();
-  Eigen::Vector3d y = z.cross(x).normalized();
-
-  auto p = Eigen::Isometry3d::Identity();
-  p.translation() = origin;
-  p.matrix().col(0).head<3>() = x;
-  p.matrix().col(1).head<3>() = y;
-  p.matrix().col(2).head<3>() = z;
-  return p;
+  if (!nh.getParam(key, val))
+  {
+    ROS_ERROR_STREAM("Failed to get '" << key << "' parameter");
+    return false;
+  }
+  return true;
 }
 
-
-static void computeProperties(gl_depth_sim::CameraProperties& properties,
-                         float z_near, float z_far, float resolution)
+visualization_msgs::Marker createMeshMarker(const Eigen::Isometry3d &pose,
+                                            const std::string &frame,
+                                            const std::string &mesh_resource)
 {
-  properties.z_near = z_near;
-  properties.z_far = z_far;
-  float fov_x = 2*M_PI/3;
-  properties.width = ceil(fov_x / resolution);
-  properties.height  = 1; //TODO is there a calculation for this?
-  properties.fx = properties.width / (2*tan(fov_x/2));
-  properties.fy = properties.fx;
-  properties.cx = properties.width / 2;
-  properties.cy = properties.height / 2;
+  visualization_msgs::Marker marker;
 
+  marker.header.frame_id = frame;
+  marker.id = 0;
+  marker.ns = "mesh";
+  marker.action = visualization_msgs::Marker::ADD;
+
+  marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+
+  // Check if the mesh resource does not use the "file://" or "package://" URI
+  if (mesh_resource.find("file://") == std::string::npos
+      && mesh_resource.find("package://") == std::string::npos)
+  {
+    // Assume that the provided mesh resource is a fully specified file path; pre-pend the "file://" URI
+    marker.mesh_resource = "file://" + mesh_resource;
+  }
+  else
+  {
+    // The input mesh resource already has the correct URI specification
+    marker.mesh_resource = mesh_resource;
+  }
+
+  marker.scale.x = 1.0;
+  marker.scale.y = 1.0;
+  marker.scale.z = 1.0;
+
+  marker.color.a = 1.0;
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+
+  marker.pose = tf2::toMsg(pose);
+
+  return marker;
 }
-
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "laser_node");
-  ros::NodeHandle nh;
+  ros::NodeHandle nh, pnh("~");
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
   ros::Publisher cloud_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>("cloud", 1);
-  tf::TransformBroadcaster broadcaster;
+  tf2_ros::TransformBroadcaster broadcaster;
   std::string base_frame = "world";
   std::string camera_frame = "camera";
-  std::string mesh_frame = "mesh";
 
-  gl_depth_sim::CameraProperties props;
-  float z_near, z_far, resolution;
-  nh.getParam("/laser_example/z_near", z_near);
-  nh.getParam("/laser_example/z_far", z_far);
-  nh.getParam("/laser_example/resolution", resolution);
-  computeProperties(props, z_near, z_far, resolution);
+  std::string mesh_filename;
+  if (!get(pnh, "mesh_filename", mesh_filename))
+    return -1;
 
-  gl_depth_sim::SimLaserScanner laser(props);
-  std::string mesh_path;
-  nh.getParam("/laser_example/path", mesh_path);
-  std::unique_ptr<gl_depth_sim::Mesh> mesh_ptr = gl_depth_sim::loadMesh(mesh_path);
-  auto mesh_pose = Eigen::Isometry3d::Identity();
-//  mesh_pose.translation() = Eigen::Vector3d(1, -1, 0);
+  // Get the laser scanner properties
+  gl_depth_sim::LaserScannerProperties laser_scan_props;
+  if (!get(pnh, "min_range", laser_scan_props.min_range)
+      || !get(pnh, "max_range", laser_scan_props.max_range)
+      || !get(pnh, "angular_resolution", laser_scan_props.angular_resolution))
+  {
+    return -1;
+  }
+
+  // Create the laser scanner
+  gl_depth_sim::SimLaserScanner laser(laser_scan_props);
+
+  // Add the mesh
+  std::unique_ptr<gl_depth_sim::Mesh> mesh_ptr = gl_depth_sim::loadMesh(mesh_filename);
+  Eigen::Isometry3d mesh_pose(Eigen::Isometry3d::Identity());
   laser.add(*mesh_ptr, mesh_pose);
 
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  cloud.header.frame_id = camera_frame;
+  // Publish a message with the mesh for visualization
+  ros::Publisher pub = nh.advertise<visualization_msgs::Marker>("object", 1, true);
+  visualization_msgs::Marker marker = createMeshMarker(mesh_pose, base_frame, mesh_filename);
+  pub.publish(marker);
 
-  /*
-//  double cam_x, cam_y, cam_z;
-//  nh.getParam("/laser_example/cam_x", cam_x);
-//  nh.getParam("/laser_example/cam_y", cam_y);
-//  nh.getParam("/laser_example/cam_z", cam_z);
-//  Eigen::Vector3d camera_pos (cam_x,cam_y,cam_z);
-//  auto camera_pose = lookat(camera_pos, Eigen::Vector3d(1,0,0), Eigen::Vector3d(0,0,1));
-  */
-  const static double radius = 2.0;
-  const auto start = std::chrono::steady_clock::now();
+  // Sweep the laser scanner back and forth across the surface of a part in the world y-axis direction
+  //
+  const double sweep_distance = 0.8;
+  Eigen::Isometry3d nominal_scanner_pose = Eigen::Isometry3d::Identity();
+  nominal_scanner_pose.translate(Eigen::Vector3d(0.0, 0.0, 2.0));
+  nominal_scanner_pose.rotate(Eigen::AngleAxisd(M_PI_2, Eigen::Vector3d::UnitX()));
+
+  std::size_t counter = 0;
+  Eigen::Isometry3d scanner_pose(nominal_scanner_pose);
   while (ros::ok())
   {
-    double dt = std::chrono::duration<double>(std::chrono::steady_clock::now() - start).count();
-    Eigen::Vector3d camera_pos (radius * cos(dt/2), radius * sin(dt/2), 2.0);
-    auto camera_pose = lookat(camera_pos,  Eigen::Vector3d(0,0,0), Eigen::Vector3d(0,0,1));
+    pcl::PointCloud<pcl::PointXYZ> scan = laser.render(scanner_pose);
 
-    const auto depth_data = laser.render(camera_pose);
-    gl_depth_sim::toPointCloudXYZ(props, depth_data, cloud);
-    pcl_conversions::toPCL(ros::Time::now(), cloud.header.stamp);
-    cloud.header.frame_id = camera_frame;
-    cloud_pub.publish(cloud);
-    tf::Transform camera_transform;
-    tf::transformEigenToTF(camera_pose, camera_transform);
-    tf::StampedTransform camera_stamped_transform (camera_transform, ros::Time::now(), base_frame, camera_frame);
-    broadcaster.sendTransform(camera_stamped_transform);
+    // Publish the scan cloud
+    scan.header.frame_id = camera_frame;
+    pcl_conversions::toPCL(ros::Time::now(), scan.header.stamp);
+    cloud_pub.publish(scan);
+
+    // Wait
+    ros::Duration(0.005).sleep();
+
+    // Update the transform
+    ++counter;
+    double z = std::sin(static_cast<double>(counter) / 180.0) * sweep_distance;
+    scanner_pose = nominal_scanner_pose * Eigen::Translation3d(Eigen::Vector3d(0.0, 0.0, z));
+
+    geometry_msgs::TransformStamped transform = tf2::eigenToTransform(scanner_pose);
+    transform.header.frame_id = base_frame;
+    transform.header.stamp = ros::Time::now();
+    transform.child_frame_id = camera_frame;
+    broadcaster.sendTransform(transform);
   }
 
   ros::waitForShutdown();
